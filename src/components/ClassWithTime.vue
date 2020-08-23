6de2598951d9ba79e4e92eff51a37191c6cde31e
Azamat-Alibaev <alibaevan3@gmail.com>
2020-08-23 17:48:18 +0500

    linked ClassWithTime component recieving class as prop

---
+++ src/components/ClassWithTime.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="class-with-time-container">
+    <div class="time">{{ this.classObject.time }}</div>
+    <div class="class" :class="classWithPosition">
+      {{ this.classObject.classCode }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    classObject: {
+      type: Object
+    }
+  },
+  computed: {
+    classWithPosition() {
+      return null
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.class-with-time-container
+    width: 100vw
+    position: relative
+
+.time
+    position: absolute
+    left: 0px
+
+.classCode
+    position: relative
+</style>
--- src/views/GlobalTime.vue
+++ src/views/GlobalTime.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <h1>Общее школьное время</h1>
+    <ClassWithTime
+      class="class"
+      v-bind:classObject="{ time: '08:00', classCode: '#1' }"
+    >
+    </ClassWithTime>
+    <ClassWithTime time="05" classCode="#2"> </ClassWithTime>
     <router-link class="router-link" :to="{ name: 'board' }"
       >Доска расписания</router-link
     >
@@ -8,7 +14,19 @@
 </template>
 
 <script>
-export default {}
+import ClassWithTime from '../components/ClassWithTime'
+export default {
+  components: {
+    ClassWithTime
+  },
+  data() {
+    return {
+      class: {
+        type: Object
+      }
+    }
+  }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="sass" scoped></style>
