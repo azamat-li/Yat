fb6dee8f6c58bb7ebf8dc8a61d3f33ff632acffe
Azamat-Alibaev <alibaevan3@gmail.com>
2020-08-27 00:17:25 +0500

    added editing lesson capability

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -5,13 +5,15 @@
       <div v-for="(day, $dayIndex) of form.days" :key="$dayIndex" class="day">
         <div class="list-reset">
           <div class="flex items-center mb-2 font-bold">{{ day.name }}</div>
-          <div
-            v-for="(lesson, $lessonIndex) of day.lessons"
-            :key="$lessonIndex"
-            class="lesson"
-            @click="goToLesson(lesson)"
-          >
-            <span class="w-full flex-no-shrink font-bold">{{ lesson.name }}</span>
+          <div v-for="(lesson, $lessonIndex) of day.lessons" :key="$lessonIndex" class="lesson">
+            <input
+              type="text"
+              class="w-full flex-no-shrink font-bold bg-transparent"
+              :value="lesson.name"
+              @keyup.enter="updateLessonProperty($event, 'name', lesson)"
+              @change="updateLessonProperty($event, 'name', lesson)"
+              placeholder="Название урока"
+            />
             <p v-if="lesson.notes" class="w-full text-no-shrink mt-1 text-sm">{{ lesson.notes }}</p>
           </div>
         </div>
@@ -42,6 +44,13 @@ export default {
     },
     goToLesson(lesson) {
       this.$router.push({ name: 'lesson', params: { id: lesson.id } })
+    },
+    updateLessonProperty(e, key, lesson) {
+      this.$store.commit('UPDATE_LESSON', {
+        lesson,
+        key,
+        value: e.target.value
+      })
     }
   }
 }
@@ -52,7 +61,7 @@ export default {
   @apply pin absolute;
 }
 .lesson {
-  @apply flex items-center text-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
+  @apply flex items-center text-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-transparent text-grey-darkest no-underline;
 }
 
 .day {
