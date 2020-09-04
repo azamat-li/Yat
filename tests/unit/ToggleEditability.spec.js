486882374d261ae481061bd43ee480c75696a2e4
Azamat-Alibaev <alibaevan3@gmail.com>
2020-09-04 12:07:42 +0500

    added ToggleEditability.spec.js test that @click 'toggle-editability' is emitted

--- tests/unit/ToggleEditability.spec.js
+++ tests/unit/ToggleEditability.spec.js
@@ -2,8 +2,18 @@ import ToggleEditability from '@/components/ToggleEditability'
 import { mount } from '@vue/test-utils'
 
 describe('ToggleEditability', () => {
-  it('If isEditable is true, Рады изменениям div is seen', () => {
+  it('At button click emits "toggle-editability"', async () => {
     const wrapper = mount(ToggleEditability)
-    expect(true).toBe(true)
-  })
+
+    wrapper.find('button').trigger('click')
+
+    wrapper.vm.$nextTick()
+
+    const buttonClickCalls = wrapper.emitted('toggle-editability')
+    expect(buttonClickCalls).toHaveLength(1)
+  }),
+    it('If isEditable is true, Рады изменениям div is seen', () => {
+      const wrapper = mount(ToggleEditability)
+      expect(true).toBe(true)
+    })
 })
