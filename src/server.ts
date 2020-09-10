9d0f5019b578cf43bbef320a4b9c8a2816d8a0d4
Azamat-Alibaev <alibaevan3@gmail.com>
2020-09-11 02:24:35 +0500

    changed server.ts to use heroku's app/dist path

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,12 @@ import serveStatic from 'serve-static'
 
 const app = express()
 
-
-
 // here we are configuring dist to serve app files
-app.use('/', serveStatic( './dist'))
+app.use('/', serveStatic('app/dist'))
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, (req, res) => {
-  res.sendFile('./dist/index.html')
+  res.sendFile('app/dist/index.html')
 })
 
 const port = process.env.PORT || 8080
