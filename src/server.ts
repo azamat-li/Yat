d66e05704fdd2aa4f09765b1fb084a9939ea7140
Azamat-Alibaev <alibaevan3@gmail.com>
2020-09-11 02:29:46 +0500

    changed server.ts to serve from dist path

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,11 @@ import serveStatic from 'serve-static'
 const app = express()
 
 // here we are configuring dist to serve app files
-app.use('/', serveStatic('app/dist'))
+app.use('/', serveStatic('dist'))
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, (req, res) => {
-  res.sendFile('app/dist/index.html')
+  res.sendFile('dist/index.html')
 })
 
 const port = process.env.PORT || 8080
