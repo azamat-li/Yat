91783d1d93d8bf0cf5b1408b79ee9169629986e1
Azamat-Alibaev <alibaevan3@gmail.com>
2020-10-08 19:27:56 +0500

    refactored Form.vue to use flex flex-wrap as  FormBlock + input wrapper

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,35 +1,41 @@
 <template>
   <div class="top-0 w-full rounded-lg absolute">
-    <div v-if="form.name" class="h-8 w-full">
-      <div draggable class="header bg-gray-700 text-gray-200 w-56 rounded-full">
-        {{ form.name }}
+    <div class=" flex flex-wrap">
+      <div v-if="form.name" class="h-8 w-full">
+        <div
+          draggable
+          class="header bg-gray-700 text-gray-200 w-56 rounded-full"
+        >
+          {{ form.name }}
+        </div>
+      </div>
+
+      <div
+        class="flex flex-row  flex-shrink items-start bg-gray-800 text-gray-200 "
+      >
+        <FormBlock
+          v-for="(day, $dayIndex) of form.days"
+          :key="$dayIndex"
+          :block="day"
+          :blockIndex="$dayIndex"
+          :form="form"
+          :isEditable="isFormEditable"
+          blockType="day"
+          class="flex-shrink"
+        />
+        <div v-if="isFormEditable" class=" bg-gray-800">
+          <input
+            type="text"
+            class="bg-transparent   text-gray-300 "
+            placeholder="+"
+            @keyup.enter="createBlock($event)"
+          />
+        </div>
       </div>
     </div>
 
-    <div
-      class="flex flex-row flex-shrink items-start bg-gray-800 text-gray-200 "
-    >
-      <FormBlock
-        v-for="(day, $dayIndex) of form.days"
-        :key="$dayIndex"
-        :block="day"
-        :blockIndex="$dayIndex"
-        :form="form"
-        :isEditable="isFormEditable"
-        blockType="day"
-        class="flex-shrink"
-      />
-      <div v-if="isFormEditable" class=" bg-gray-800">
-        <input
-          type="text"
-          class="bg-transparent   text-gray-300 "
-          placeholder="+"
-          @keyup.enter="createBlock($event)"
-        />
-      </div>
-    </div>
     <ToggleEditability
-      class=" text-left  bg-gray-800 text-gray-300"
+      class=" text-left   bg-gray-800 text-gray-300"
       editableStateString="isFormEditable"
     ></ToggleEditability>
   </div>
